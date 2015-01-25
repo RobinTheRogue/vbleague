94d1dfe277d98eb60d7d527ba221983d55883323
Robin <robin@rogue-coders.com>
2015-01-25 16:09:42 -0700

    add News CRUD for homepage news

---
+++ app/controllers/news_controller.rb
@@ -0,0 +1,37 @@
+class NewsController < ApplicationController
+  def index
+    @news = News.order(created_at: :desc)
+  end
+
+  def show
+    @news = News.find(params[:id])
+  end
+
+  def edit
+    @news = News.find(params[:id])
+  end
+
+  def update
+    @news = News.find(params[:id])
+    news_params = params.require(:news).permit(:title, :body)
+    @news.update(news_params)
+    redirect_to @news
+  end
+
+  def new
+    @news = News.new
+  end
+
+  def create
+    news_params = params.require(:news).permit(:title, :body)
+    @news = News.new(news_params)
+    @news.save
+    redirect_to @news
+  end
+
+  def destroy
+    @news = News.find(params[:id])
+    @news.destroy
+    redirect_to news_url
+  end
+end
